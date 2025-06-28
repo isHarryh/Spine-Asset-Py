688e6229d6a568db370f163980e7faa959474b32
Harry Huang <harryhuang2652@qq.com>
2025-06-29 02:04:28 +0800

    ci: add test workflow

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
+import sys
+
 from tests import test_v38
 
 if __name__ == "__main__":
-    test_v38.test_batch("tests/v38")
+    is_fail_fast = "--fail-fast" in sys.argv
+
+    if not test_v38.test_batch("tests/v38") and is_fail_fast:
+        sys.exit(1)

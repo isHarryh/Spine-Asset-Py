28023c6b4d292cc3fafdd5994151df18bc3bfd04
Harry Huang <harryhuang2652@qq.com>
2025-06-30 08:35:01 +0800

    chore: add missing imports

--- spine_asset/v38/__init__.py
+++ spine_asset/v38/__init__.py
@@ -3,12 +3,27 @@ from . import timelines
 from .Animation import Animation
 from .BoneData import BoneData
 from .ConstraintData import ConstraintData
-from .Enums import BlendMode, TransformMode, PositionMode, SpacingMode, RotateMode
+from .Enums import (
+    BlendMode,
+    AttachmentType,
+    BinaryAttachmentType,
+    BinaryTimelineType,
+    BinaryCurveType,
+    PositionMode,
+    SpacingMode,
+    RotateMode,
+    TransformMode,
+    MixBlend,
+    MixDirection,
+    TimelineType,
+)
 from .EventData import EventData
 from .IkConstraintData import IkConstraintData
 from .LinkedMesh import LinkedMesh
 from .PathConstraintData import PathConstraintData
+from .SkeletonBinary import SkeletonBinary
 from .SkeletonData import SkeletonData
+from .SkeletonJson import SkeletonJson
 from .Skin import Skin, SkinEntry
 from .SlotData import SlotData
 from .TransformConstraintData import TransformConstraintData
@@ -19,16 +34,25 @@ __all__ = [
     "attachments",
     "timelines",
     "Animation",
+    "AttachmentType",
+    "BinaryAttachmentType",
+    "BinaryTimelineType",
+    "BinaryCurveType",
     "BlendMode",
     "BoneData",
     "ConstraintData",
+    "MixBlend",
+    "MixDirection",
+    "TimelineType",
     "EventData",
     "IkConstraintData",
     "LinkedMesh",
     "PathConstraintData",
     "PositionMode",
     "RotateMode",
+    "SkeletonBinary",
     "SkeletonData",
+    "SkeletonJson",
     "Skin",
     "SkinEntry",
     "SlotData",
